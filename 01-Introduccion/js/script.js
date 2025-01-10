f3b7d46ca92e7bf13519dd94d8f6b9d5ad7f3bb0
Markin Piero <markinpiero005@gmail.com>
2025-01-10 10:31:39 -0500

    seccion 1 finalizada

---
+++ 01-Introduccion/js/script.js
@@ -0,0 +1,19 @@
+// alert("Hola mundo")
+
+// const nombre = prompt("Cuál es tú nombre");
+// document.querySelector(".contenido").innerHTML= `${nombre} está aprendiendo JavaScript moderno`;
+
+//Comentario de UNA sola línea
+
+/* Comentarios de
+MÚLTIPLES líneas */
+
+console.log("Hola mundo");
+
+
+console.warn("Eso no está permitido");
+console.warn("Eso no está permitido");
+console.warn("Eso no está permitido");
+console.warn("Eso no está permitido");
+
+console.timeEnd("Hola")
