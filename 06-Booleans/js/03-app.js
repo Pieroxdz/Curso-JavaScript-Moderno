ac8fcdcd91cd7d0adf7c07683b976441428f11c3
Markin Piero <markinpiero005@gmail.com>
2025-01-10 16:43:53 -0500

    seccion 6 finalizada

---
+++ 06-Booleans/js/03-app.js
@@ -0,0 +1,9 @@
+const autenticado = false;
+
+if(autenticado){
+    console.log("Si puedes ver netflixt");
+}else{
+    console.log("No no puedes verlo");
+}
+
+console.log(autenticado ? "Si está autenticado" : "No está autenticado");
