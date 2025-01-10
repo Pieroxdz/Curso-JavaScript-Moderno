a6ddc6fa2f8d4f465aa7fd5683f2af91488b63e5
Markin Piero <markinpiero005@gmail.com>
2025-01-10 14:44:29 -0500

    seccion 4 finalizada

---
+++ 04-Numeros/js/01-app.js
@@ -0,0 +1,15 @@
+//Creación de un número
+
+const num1 = 30;
+const num2 = .10220;
+const num3 = 20.5;
+const num4 = .15;
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+
+const num5 = new Number(80)
+console.log(num5);
+
---
+++ 04-Numeros/js/03-app.js
@@ -0,0 +1,38 @@
+let resultado;
+
+//PI
+resultado = Math.PI
+
+//Redondear de forma tradicional
+resultado = Math.round(2.7)
+
+//Redodear siempre al entero mayor
+resultado = Math.ceil(2.1)
+
+
+//Redondear siempre al entero menor
+resultado = Math.floor(2.8)
+
+//Raíz cuadrada
+resultado = Math.sqrt(144);
+
+//Valor absoluto
+resultado = Math.abs(-500) //Debe ser 500
+
+//Potencia
+resultado = Math.pow(3, 3);
+
+//Minimo
+resultado = Math.min(3, 5, 10, 1, -3);
+
+//Máximo
+resultado = Math.max(2, 8, 10, 100);
+
+//Numero aleatorio
+resultado = Math.round(Math.random() * 10);
+
+
+
+
+
+console.log(resultado);
